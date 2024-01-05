ea5edee1f314e4ab944ce4900b89b9658852acad
gyojinnK <ksmug1998@gmail.com>
2024-01-05 10:39:48 +0900

    Time: 1528 ms (5.01%), Space: 17.5 MB (11.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,7 +2,7 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # 탐색을 위한 투포인터
         checker = []
-        head, rear, longest = 0, 0, 0
+        head = rear = longest = 0
         
         while rear < len(s):
             # 중복(재등장) 확인을 위함
